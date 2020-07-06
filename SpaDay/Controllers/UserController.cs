707aac6c5409ac51a4179e52c9275ffe7cdc2c03
thinkrealhard <allen.touchstone@gmail.com>
2020-07-05 15:07:54 -1000

    Updates

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if(newUser.Password == verify)
+            {
+                ViewBag.user = newUser;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.user = newUser;
+                ViewBag.error = "Passwords didn't match!";
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public User()
+        {
+            Timestamp = DateTime.Now;
+        }
+        public User(string username, string email, string password) : this()
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+    }
+}
